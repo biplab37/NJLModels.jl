ac9aa3f27a2776bc963e0aefb636597b17d88958
biplab37 <biplabmahato37@gmail.com>
2026-07-05 22:17:34 +0200

    added devurl

--- docs/make.jl
+++ docs/make.jl
@@ -11,6 +11,7 @@ makedocs(
     format = DocumenterVitepress.MarkdownVitepress(
         repo = "https://github.com/biplab37/NJLModels.jl.git",
         devbranch="main",
+        devurl="",
     ),
     warnonly = [:missing_docs],
 )
