fdae56447cf559618a4d77ca3f6d039a4fc34cc9
biplab37 <biplabmahato37@gmail.com>
2026-07-05 22:55:56 +0200

    fixed docs issues

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,26 @@
 using Documenter, NJLModels, DocumenterVitepress
 
 makedocs(
-    modules  = [NJLModels],
-    sitename = "NJLModels",
-    authors  = "Biplab Mahato",
+    modules=[NJLModels],
+    sitename="NJLModels",
+    authors="Biplab Mahato",
     doctest=false,
-    pages    = [
-        "Home"         => "index.md",
+    pages=[
+        "Home" => "index.md",
     ],
-    format = DocumenterVitepress.MarkdownVitepress(
-        repo = "https://github.com/biplab37/NJLModels.jl",
+    format=DocumenterVitepress.MarkdownVitepress(
+        repo="https://github.com/biplab37/NJLModels.jl",
         devbranch="main",
-        devurl = "dev",
-        deploy_url = "https://biplab37.github.io/NJLModels.jl/dev/",
+        devurl="dev",
+        deploy_url="https://biplab37.github.io/NJLModels.jl/dev/",
     ),
-    warnonly = [:missing_docs],
+    warnonly=[:missing_docs],
 )
 
 DocumenterVitepress.deploydocs(
-    repo = "https://github.com/biplab37/NJLModels.jl.git",
-    target = joinpath(@__DIR__, "build"),
-    branch = "gh-pages",
-    devbranch = "main",
-    push_preview = true,
+    repo="github.com/biplab37/NJLModels.jl",
+    target=joinpath(@__DIR__, "build"),
+    branch="gh-pages",
+    devbranch="main",
+    push_preview=true,
 )
