498d3e9805318fab0ead576580061f79cf8da27a
biplab37 <biplabmahato37@gmail.com>
2026-07-06 12:14:54 +0200

    fixed docs issues 2

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,18 @@ using Documenter, NJLModels, DocumenterVitepress
 
 makedocs(
     modules=[NJLModels],
-    sitename="NJLModels",
+    sitename="NJLModels.jl",
     authors="Biplab Mahato",
     doctest=false,
+    repo=Remotes.GitHub("biplab37", "NJLModels.jl"),
     pages=[
         "Home" => "index.md",
     ],
     format=DocumenterVitepress.MarkdownVitepress(
         repo="https://github.com/biplab37/NJLModels.jl",
         devbranch="main",
-        devurl="dev",
-        deploy_url="https://biplab37.github.io/NJLModels.jl/dev/",
+        # devurl="dev",
+        # deploy_url="https://biplab37.github.io/NJLModels.jl/dev/",
     ),
     warnonly=[:missing_docs],
 )
